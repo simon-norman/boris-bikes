ee63342d8f517bb72aea89d180bfcf7826833493
“Brenda” <odonnell.brenda@gmail.com>
2019-02-05 17:08:23 +0000

    respond_to test created

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -1,4 +1,11 @@
 # In spec/dockingstation_spec.rb
+require '../lib/dockingstation.rb'
 describe DockingStation do
+it "responds_to release_bike method" do
+  dockingstation.release_bike
+  expect(dockingstation).to respond_to(:release_bike)
+end
+
+
 
 end
