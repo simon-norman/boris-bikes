3096bc119a21450ee9dfcd13861a89feaf1f3695
Simon <simon@Admins-MacBook-Pro.local>
2019-02-05 17:18:13 +0000

    Fixed test to check that dockingstation has release bike method

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -1,11 +1,9 @@
 # In spec/dockingstation_spec.rb
-require '../lib/dockingstation.rb'
+require './lib/dockingstation.rb'
+
 describe DockingStation do
-it "responds_to release_bike method" do
-  dockingstation.release_bike
-  expect(dockingstation).to respond_to(:release_bike)
-end
-
-
-
+  it 'responds_to release_bike method' do
+    dockingstation = DockingStation.new
+    expect(dockingstation).to respond_to(:release_bike)
+  end
 end
